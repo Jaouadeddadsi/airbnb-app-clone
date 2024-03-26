45c4af14ef213a37ed8f727a8e0b18889ae028e9
Jaouad Eddadsi <jaouadeddadsi2016@gmail.com>
2024-03-26 11:55:18 +0000

    Add imgs hostnames to config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
+      {
+        protocol: "https",
         hostname: "res.cloudinary.com",
         port: "",
       },
